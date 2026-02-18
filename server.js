45992d003f7ae849197d1d8e8bcd112796189818
Aditya K <pjakeerhussain5069301@gmail.com>
2026-02-18 20:30:51 +0530

    code: Updated packages.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/errorMiddleware");
+require("dotenv").config();
 
 console.log("server.js");
 
