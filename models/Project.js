0e49778fd2d6bfed4bf2d72cd288715c65eaf95e
Aditya K <pjakeerhussain5069301@gmail.com>
2026-02-18 22:00:07 +0530

    code: added models

---
+++ models/Project.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: String,
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Project", projectSchema);
